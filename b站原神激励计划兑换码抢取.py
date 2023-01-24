ad9c4953ef730e1522cdc2e080ccde6c3f574d34
guoluyuan <53471491+guoluyuan@users.noreply.github.com>
2023-01-25 01:28:53 +0800

    Add files via upload

---
+++ b站原神激励计划兑换码抢取.py
@@ -0,0 +1,83 @@
+# @Author: 郭盖
+# @FileName: tanchishe.py
+# @DateTime: 2022/12/11 15:53
+# @SoftWare:  PyCharm
+import multiprocessing
+import requests
+import time
+#谷歌浏览器中
+# CSRF，ID在你要领取的直播奖励界面点进去进去F12再F5刷新看到网络中有一个链接(https://api.bilibili.com/x/activity/mission/single_task?csrf=xx&id=xx)可以看到
+csrf = "1944912c99e6e3d45046e3c985327c45"
+id = "cc7955bf"
+cookie = "buvid4=86AA846A-9718-7FCB-D26C-5E0EB1029C3A94107-022100810-GpNK3Dg1dBUm7qq2WlrxkA%3D%3D; b_nut=1665195694; buvid3=F76122D9-FE80-11D9-AAE5-9D77C0AF990694625infoc; i-wanna-go-back=-1; _uuid=1CA5E16D-3CAE-45102-CF4A-BB610EF348D8895450infoc; DedeUserID=18767576; DedeUserID__ckMd5=b4c0188a55412cd0; b_ut=5; nostalgia_conf=-1; balh_is_closed=; balh_server_inner=__custom__; LIVE_BUVID=AUTO3716665271113309; is-2022-channel=1; blackside_state=1; fingerprint3=03ae031d387df1ddba61387c3c71b90d; CURRENT_BLACKGAP=0; buvid_fp_plain=undefined; rpdid=|(kmm~~Ruu)u0J'uYY)~k)Ym|; CURRENT_QUALITY=120; fingerprint=42acf6843cd02953bd9efb94c3eb2d4b; CURRENT_FNVAL=4048; SESSDATA=ddaacf94%2C1690073965%2C152ea%2A12; bili_jct=1944912c99e6e3d45046e3c985327c45; sid=54u6fw2f; PVID=4; b_lsid=3B5E734F_185E40AE0DF; innersign=1; bp_video_offset_18767576=754768802317598700; buvid_fp=42acf6843cd02953bd9efb94c3eb2d4b"
+# 奖励领取资格查询
+def isCan():
+    """
+    :return:!0有资格
+            0无资格
+    """
+    url = f"https://api.bilibili.com/x/activity/mission/single_task?csrf={csrf}&id={id}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+        "cookie": cookie,
+    }
+    res = requests.get(url, headers=headers).json()
+    flage = res['data']['task_info']['receive_id']
+    # print(res['data']['task_info'])
+    if flage>0:
+        return res['data']['task_info']
+    else:
+        return 0
+# print(isCan())
+def getaword():
+    info=isCan()
+    url = "https://api.bilibili.com/x/activity/mission/task/reward/receive"
+    headers = {
+        "User-Agent": "",
+        "cookie": cookie,
+    }
+    data = {
+        "csrf": csrf,
+        "receive_from": "missionLandingPage",
+        "receive_id": info['receive_id'],
+        "group_id": info['group_list'][0]['group_id'],
+        "task_id": info['id'],
+        "act_id": info['act_id'],
+    }
+    print(data)
+    # data={
+    #     "csrf":CSRF,
+    #     "receive_from":"missionLandingPage",
+    #     "receive_id":685407,
+    #     "group_id":0,
+    #     "task_id":2253,
+    #     "act_id":586,
+    # }
+    res=requests.post(url,headers=headers,data=data).json()
+    print(res)
+    # pass
+
+
+if __name__ == '__main__':
+    result=None
+    while(True):
+        result=isCan()
+        if(result!=0):
+            print("已经获取到资格")
+            break
+        time_str=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print(time_str+":等待领取资格")
+        time.sleep(1)
+    getaword()
+    time1 = time.perf_counter()
+    #线程数
+    pool = multiprocessing.Pool(10)
+    #一共跑几次
+    task_number = 100
+    for i in range(task_number):
+        pool.apply_async(func=getaword, args=())
+    pool.close()
+    pool.join()
+    time2 = time.perf_counter()
+    times = time2 - time1
+    print(times / task_number)  # 每次请求用时
